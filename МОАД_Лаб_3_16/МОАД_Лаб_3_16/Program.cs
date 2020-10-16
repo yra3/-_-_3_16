d387021d5bde5c95e0cac3d902c5029698580588
yra3 <64928855+yra3@users.noreply.github.com>
2020-10-16 20:12:33 +0500

    Added main folder with c# project

---
+++ МОАД_Лаб_3_16/МОАД_Лаб_3_16/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace МОАД_Лаб_3_16
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Rings allRings = new Rings();
+            allRings.ReadFromFile();
+            List<int> averageTimeByOperators = allRings.GetAverageTimeByOperators();
+            List<int> averageTimeByTopic = allRings.GetAverageTimeByTopic();
+            List <List<int>> averageTimeByTopicAndOperator = allRings.GetAverageTimeByTopicAndOperator();
+            List<int> workTimes = allRings.GetWorkTimes();
+            List<List<int>> averageGradeByTopicAndOperator = allRings.GetAverageGradeByTopicAndOperator();
+            List<List<int>> modeGradeByTopicAndOperator = allRings.GetModeGradeByTopicAndOperator();
+            List<List<int>> medianGradeByTopicAndOperator = allRings.GetMedianGradeByTopicAndOperator();
+
+        }
+    }
+}
---
+++ МОАД_Лаб_3_16/МОАД_Лаб_3_16/Rings.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace МОАД_Лаб_3_16
+{
+    internal class Rings
+    {
+        private List<List<int>> RingsTable = new List<List<int>>();
+        internal void ReadFromFile()
+        {
+            StreamReader streamReader = new StreamReader("Task2 1.csv");
+            List<string> operator1time = 
+        }
+
+        internal List<int> GetAverageTimeByOperators()
+        {
+            throw new NotImplementedException();
+        }
+
+        internal List<int> GetAverageTimeByTopic()
+        {
+            throw new NotImplementedException();
+        }
+
+        internal List<List<int>> GetAverageTimeByTopicAndOperator()
+        {
+            throw new NotImplementedException();
+        }
+
+        internal List<int> GetWorkTimes()
+        {
+            throw new NotImplementedException();
+        }
+
+        internal List<List<int>> GetAverageGradeByTopicAndOperator()
+        {
+            throw new NotImplementedException();
+        }
+
+        internal List<List<int>> GetModeGradeByTopicAndOperator()
+        {
+            throw new NotImplementedException();
+        }
+
+        internal List<List<int>> GetMedianGradeByTopicAndOperator()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
