8a62735d43baa499898acc37afbc39cbd5c353d3
yra3 <64928855+yra3@users.noreply.github.com>
2020-10-17 02:59:40 +0500

    Added answer writer to file and to console(3 files). Corrected metod which find average time by operator and topic.

---
+++ МОАД_Лаб_3_16/МОАД_Лаб_3_16/AnswerWriterToConsole.cs
@@ -0,0 +1,14 @@
+﻿namespace МОАД_Лаб_3_16
+{
+    internal class AnswerWriterToConsole : AnswerWriter
+    {
+        override public void WriteAnswer()
+        {
+            CalculateResponse();
+            foreach (string row in Answer)
+            {
+                System.Console.WriteLine(row);
+            }
+        }
+    }
+}
--- МОАД_Лаб_3_16/МОАД_Лаб_3_16/Rings.cs
+++ МОАД_Лаб_3_16/МОАД_Лаб_3_16/Rings.cs
@@ -10,23 +10,137 @@ namespace МОАД_Лаб_3_16
         internal void ReadFromFile()
         {
             StreamReader streamReader = new StreamReader("Task2 1.csv");
-            List<string> operator1time = 
+            streamReader.ReadLine();
+            int[] startRing = new int[5];
+            do
+            {
+                string row = streamReader.ReadLine();
+                string[] words = row.Split(';');
+                int numberOperator =Convert.ToInt32(words[0]);
+                string[] timeStrings = words[2].Split(':');
+
+                int[] timeNumbers = { Convert.ToInt32(timeStrings[0]), Convert.ToInt32(timeStrings[1]), Convert.ToInt32(timeStrings[2]) };
+                
+                int time = timeNumbers[0]*3600+timeNumbers[1]*60+timeNumbers[2];
+                
+
+                if (words[1].Trim() == "on")
+                {
+                    startRing[numberOperator - 1] = time;
+                }
+                else
+                {
+                    int timeRing = time - startRing[numberOperator-1];
+                    int topic = Convert.ToInt32(words[3]);
+                    int grade = Convert.ToInt32(words[4]);
+
+                    List<int> ring = new List<int> { numberOperator, timeRing, topic, grade };
+                    RingsTable.Add(ring);
+                }
+            }
+            while (!streamReader.EndOfStream);
         }
 
-        internal List<int> GetAverageTimeByOperators()
+
+
+
+
+
+
+        internal List<double> GetAverageTimeByOperators()
         {
-            throw new NotImplementedException();
+            List<int> time = new List<int>();
+            List<int> counts = new List<int>();
+            int count = 0;
+            int value = 0;
+            for (int k = 1; k < 6; k++)
+            {
+                for (int i = 0; i < RingsTable.Count; i++)
+                {
+                    if (RingsTable[i][0] == k)
+                    {
+                        value += RingsTable[i][1];
+                        count++;
+                    }
+                }
+                time.Add(value);
+                counts.Add(count);
+            }
+            List<double> midTime = new List<double>();
+            for (int i = 0; i < 5; i++)
+            {
+                double midTm = (double)time[i] / counts[i];
+                midTime.Add(midTm);
+            }
+            return midTime;
         }
 
-        internal List<int> GetAverageTimeByTopic()
+        internal List<double> GetAverageTimeByTopic()
         {
-            throw new NotImplementedException();
+            List<int> time = new List<int>();
+            List<int> counts = new List<int>();
+            int count = 0;
+            int value = 0;
+            for (int k = 0; k < 6; k++)
+            {
+                for (int i = 0; i < RingsTable.Count; i++)
+                {
+                    if (RingsTable[i][2] == k)
+                    {
+                        value += RingsTable[i][1];
+                        count++;
+                    }
+                }
+                time.Add(value);
+                counts.Add(count);
+            }
+            List<double> midTime = new List<double>();
+            for (int i = 0; i < 5; i++)
+            {
+                double midTm = (double)time[i] / counts[i];
+                midTime.Add(midTm);
+            }
+            return midTime;
         }
 
-        internal List<List<int>> GetAverageTimeByTopicAndOperator()
+        internal List<List<double>> GetAverageTimeByTopicAndOperator()
         {
-            throw new NotImplementedException();
+            List<List<int>> time = new List<List<int>>(5);
+
+            for (int i = 0; i < 5; i++)
+            {
+                time.Add(new List<int> { 0, 0, 0, 0, 0, 0 });
+            }
+            List<List<int>> counts = new List<List<int>>(5);
+            for (int i = 0; i < 5; i++)
+            {
+                counts.Add(new List<int> { 0, 0, 0, 0, 0, 0 });
+            }
+
+            for (int i = 1; i < RingsTable.Count; i++)
+            {
+                time[RingsTable[i][0]-1][RingsTable[i][2]] += RingsTable[i][1];
+                counts[RingsTable[i][0]-1][RingsTable[i][2]]++;
+            }
+
+            List<List<double>> midTime = new List<List<double>>();
+            for (int i = 0; i < 5; i++)
+            {
+                midTime.Add(new List<double> { 0, 0, 0, 0, 0, 0 });
+            }
+
+            for (int i = 0; i < 5; i++)
+            {
+                for (int j = 0; j < 6; j++)
+                {
+                    midTime[i][j] = (double)time[i][j] / counts[i][j];
+                }
+            }
+            return midTime;
         }
+
+
+        
 
         internal List<int> GetWorkTimes()
         {
